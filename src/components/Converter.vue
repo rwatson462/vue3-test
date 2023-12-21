c23030175d6eb358552371bf9618decc1270b6da
Rob Watson <source.pot.dev@gmail.com>
2023-12-21 19:11:56 +0000

    Dark mode

--- src/components/Converter.vue
+++ src/components/Converter.vue
@@ -9,12 +9,12 @@ const kilos = computed(() => Math.round((stones.value * 14 + pounds.value) / 2.2
 <template>
   <form class="max-w-sm mx-auto grid grid-cols-2 gap-y-4">
     <label class="flex items-center">Stones</label>
-    <input type="number" v-model.number="stones" class="text-slate-900 border border-slate-900 px-2 py-1 rounded w-full" />
+    <input type="number" v-model.number="stones" class="text-white border border-slate-800 bg-slate-700 px-2 py-1 rounded w-full" />
 
     <label class="flex items-center">Pounds</label>
-    <input type="number" v-model.number="pounds" class="text-slate-900 border border-slate-900 px-2 py-1 rounded w-full" />
+    <input type="number" v-model.number="pounds" class="text-white border border-slate-800 bg-slate-700 px-2 py-1 rounded w-full" />
 
     <label class="flex items-center">Kilos</label>
-    <input type="number" v-model.number="kilos" class="text-slate-900 border border-slate-900 px-2 py-1 rounded w-full" />
+    <input type="number" v-model.number="kilos" class="text-white border border-slate-800 bg-slate-700 px-2 py-1 rounded w-full" />
   </form>
 </template>
