bccb50d6705a01a48f80e239f31a315e50a4a1d6
Rob Watson <source.pot.dev@gmail.com>
2023-12-21 19:30:06 +0000

    Add Vuetify components

--- src/components/Converter.vue
+++ src/components/Converter.vue
@@ -7,14 +7,13 @@ const kilos = computed(() => Math.round((stones.value * 14 + pounds.value) / 2.2
 </script>
 
 <template>
-  <form class="max-w-sm mx-auto grid grid-cols-2 gap-y-4">
-    <label class="flex items-center">Stones</label>
-    <input type="number" v-model.number="stones" class="text-white border border-slate-800 bg-slate-700 px-2 py-1 rounded w-full" />
+  <v-card title="Weight converter" subtitle="Stones and Pounds to Kilos" elevation="12">
+  <form class="max-w-sm mx-auto flex flex-col">
+    <v-text-field label="Stones" type="number" v-model.number="stones" />
 
-    <label class="flex items-center">Pounds</label>
-    <input type="number" v-model.number="pounds" class="text-white border border-slate-800 bg-slate-700 px-2 py-1 rounded w-full" />
+    <v-text-field label="Pounds" type="number" v-model.number="pounds" />
 
-    <label class="flex items-center">Kilos</label>
-    <input type="number" v-model.number="kilos" class="text-white border border-slate-800 bg-slate-700 px-2 py-1 rounded w-full" />
+    <v-text-field label="Kilos" type="number" v-model.number="kilos" />
   </form>
+  </v-card>
 </template>
