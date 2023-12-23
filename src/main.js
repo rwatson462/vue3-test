7cc22f0b6e8e4614c60fd8aa1b25b2bb8e1afd48
Rob Watson <source.pot.dev@gmail.com>
2023-12-23 21:23:29 +0000

    Add Pinia store

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import './style.css'
 import App from './App.vue'
 import {Router} from "./routing/Router.js";
 import {Vuetify} from "./Vuetify.js";
+import {createPinia} from "pinia";
+
+const Pinia = createPinia()
 
 createApp(App)
     .use(Vuetify)
+    .use(Pinia)
     .use(Router)
     .mount('#app')
