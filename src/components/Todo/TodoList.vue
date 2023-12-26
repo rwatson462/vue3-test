dba77a629689197156ad2acb04a4187e56610816
Rob Watson <source.pot.dev@gmail.com>
2023-12-26 15:20:37 +0000

    Minor view refactor

--- src/components/Todo/TodoList.vue
+++ src/components/Todo/TodoList.vue
@@ -6,8 +6,8 @@
 
 <template>
   <p>Showing {{ todoStore.todoCount }} todos</p>
-  <v-list v-if="todoStore.todoCount > 0">
-    <v-list-item v-for="(todo,idx) of todoStore.todos" :key="idx" :title="todo.title" />
+  <v-list v-if="todoStore.todoCount > 0" class="bg-transparent">
+    <v-list-item v-for="(todo,idx) of todoStore.todos" :key="idx" :title="todo.title" class="rounded my-2 bg-grey-darken-4" />
   </v-list>
   <p v-else>You have nothing to do!</p>
 </template>
