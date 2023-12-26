e242faf0f543cfa9a0686f7685a3ec933d88d4a6
Rob Watson <source.pot.dev@gmail.com>
2023-12-26 15:24:47 +0000

    Add more content to home page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -6,5 +6,20 @@
   <section class="flex flex-col space-y-4 py-4">
     <p>This is a demo of Vue3 so I can learn all about how it works</p>
     <p>Click the links above to navigate around the pages</p>
+    <v-sheet class="p-4">
+      <p>Features:</p>
+      <ul class="pl-4 list-disc">
+        <li>Weight converter using a Pinia store to share state between components</li>
+        <li>
+          User registration and authentication using Supabase for Auth persistence, and a Pinia store to manage that
+          in the app
+        </li>
+        <li>Routing including restricted routes for logged-in users only (using Vue router)</li>
+        <li>
+          Simple Todo list - Create Todos, view the list of them.  Created using Pinia store backed by Supabase
+          database.  Automatically "tenanted" to the logged in user.
+        </li>
+      </ul>
+    </v-sheet>
   </section>
 </template>
